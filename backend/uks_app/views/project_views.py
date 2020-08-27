2564e9c25f77a1d4c30d973992c1cc8c8183f56c
DejanS24 <dejans1224@gmail.com>
2020-08-27 22:34:00 +0200

    Added communication with github api and cors allow settings

--- backend/uks_app/views/project_views.py
+++ backend/uks_app/views/project_views.py
@@ -1,21 +1,23 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, authentication_classes
 from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..serializers import ProjectSerializer
 from ..models import *
-# import requests
+import requests
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-@permission_classes([IsAuthenticated])
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def project(request, project_id):
     projects = Project.objects.get(id=project_id)
 
@@ -24,23 +26,24 @@ def project(request, project_id):
     return Response(serializer.data)
 
 
-@login_required
 @api_view(['GET'])
-def projects(request):
-    projects = Project.objects.all()
-    serializer = ProjectSerializer(projects, many=True)
-    # return JsonResponse(serializer.data, safe=False)
-    return Response(serializer.data)
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def projects(_, git_username):
+    repos = requests.get('https://api.github.com/users/{0}/repos'.format(git_username))
+    return HttpResponse(repos)
 
 
-@login_required
 @api_view(['GET'])
-def issues(request, project_id):
-    response = "You're looking at the issues for project %s."
-    return HttpResponse(response % project_id)
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def issues(request, git_username, project_name):
+    repo = requests.get('https://api.github.com/repos/{0}/{1}'.format(git_username, project_name))
+    return HttpResponse(repo)
 
 
-@login_required
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def issue(request, project_id, issue_id):
     return HttpResponse("You're inspecting this issue %s." % issue_id)
